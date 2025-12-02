547c653e32889e748ee90aa140929eccb25cf524
Samarjiit <samarjitmahi6245@gmail.com>
2025-12-02 14:41:36 +0530

    first commit

---
+++ transaction-service/src/main/java/com/paypal/transaction_service/dto/TransferRequest.java
@@ -0,0 +1,40 @@
+package com.paypal.transaction_service.dto;
+
+public class TransferRequest {
+    private  String senderName;
+    private  String receiverName;
+    private Double amount;
+
+    public TransferRequest() {
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public TransferRequest(String senderName, String receiverName, Double amount) {
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.amount = amount;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
---
+++ transaction-service/src/main/java/com/paypal/transaction_service/service/TransactionServiceImpl.java
@@ -0,0 +1,54 @@
+package com.paypal.transaction_service.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paypal.transaction_service.entity.Transaction;
+import com.paypal.transaction_service.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Service
+public class TransactionServiceImpl implements  TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final ObjectMapper objectMapper;
+
+    public TransactionServiceImpl(TransactionRepository transactionRepository, ObjectMapper objectMapper) {
+        this.transactionRepository = transactionRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Transaction createTransaction(Transaction request) {
+
+        System.out.println("🚀 Entered createTransaction()");
+
+        Long senderId = request.getSenderId();
+        Long receiverId = request.getReceiverId();
+        Double amount = request.getAmount();
+
+
+
+
+        Transaction transaction = new Transaction();
+        transaction.setSenderId(senderId);
+        transaction.setReceiverId(receiverId);
+        transaction.setAmount(amount);
+        transaction.setTimestamp(LocalDateTime.now());
+        transaction.setStatus("SUCCESS");
+
+        System.out.println("📥 Incoming Transaction object: " + transaction);
+
+        Transaction saved = transactionRepository.save(transaction);
+        System.out.println("💾 Saved Transaction from DB: " + saved);
+
+        return saved;
+    }
+
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+}
