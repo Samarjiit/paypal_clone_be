4c6a7041dd2d90d3b5b93e5a40280be00def4a62
Samarjiit <samarjitmahi6245@gmail.com>
2025-12-03 01:59:28 +0530

    added notification service

---
+++ notification-service/src/main/java/com/paypal/notification_service/controller/NotificationController.java
@@ -0,0 +1,25 @@
+package com.paypal.notification_service.controller;
+
+
+import com.paypal.notification_service.entity.Notification;
+import com.paypal.notification_service.service.NotificationService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    private NotificationService notificationService;
+
+    @PostMapping
+    public Notification sendNotification(@RequestBody Notification notification) {
+        return notificationService.sendNotification(notification);
+    }
+
+    @GetMapping("/{userId}")
+    public List<Notification> getNotificationsForUser(@PathVariable Long userId){
+        return notificationService.getNotificationsForUser(userId);
+    }
+}
